f61d2761eee1814a46db75ebaae7a68c31244841
Doan Quang Huy <quanghuybest@gmail.com>
2023-11-03 15:42:05 +0700

    empty Phonetics class, tests

---
+++ src/Phonetics.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Phonetic;
+
+class Phonetics
+{
+    const FORMAT_TXT = 'txt';
+    const FORMAT_ARRAY = 'array';
+    const FORMAT_JSON = 'json';
+    const LANGUAGE_DEFAULT = 'en_us';
+
+
+    public static function symbols(string $string, string $format = self::FORMAT_TXT, string $language = self::LANGUAGE_DEFAULT)
+    {
+        //
+    }
+}
---
+++ tests/PhoneticsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Phonetic;
+
+use PHPUnit\Framework\TestCase;
+
+class PhoneticsTest extends TestCase
+{
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
